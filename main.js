3be7da28c78636fe7895bd955838cff56085c7de
Krzysztof Żurowicz <k.zurowicz@datatake.pl>
2020-01-06 16:53:23 +0100

    BEM implemented

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const name = 'Krzysztof Żurowicz';
-
 console.log(`${name} wita cię na stronie kursu WTF.`);
+
+const about = document.querySelector('.about__header--js');
+console.log(about.innerHTML);
+
+about.innerHTML = 'Taka sytuacja !!!';
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[0]);
